67e7bbc7aead8d5801022b7a6b2887f0fa173a7e
Harrison Andrews <harrisonandrews1986@gmail.com>
2015-11-11 15:56:35 -0500

    added travis.yml, updated readme, and added tests.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 == README
+[![Build Status](https://travis-ci.org/HarrisonAndrews/smashitt.svg?branch=master)]
+(http://travis-ci.org/HarrisonAndrews/smashitt)
 
 This README would normally document whatever steps are necessary to get the
 application up and running.
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,3 +5,35 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
+
+#
+# test "emails must be present" do
+#   user = User.new
+#
+#   refute user.save
+#   assert user.errors[:email].present?
+#
+# end
+#
+# test "users must have a password" do
+#   user = User.new
+#
+#   refute user.save
+#   assert user.errors[:password],present?
+# end
+#
+# test "users with an email and password can be saved" do
+#   user = User.new(email: "foo@bar.com", password: "hunter2")
+#
+#   assert user.save
+#   assert user.errors.empty?
+# end
+#
+# test "user emails must be unique" do
+#   user1 = User.new(email: "foo@bar.com", password: "cookies")
+#   user2 = User.new(email: "foo@bar.com", password: "monsters")
+#
+#   assert user1.save
+#   assert user2.save
+#
+# end
