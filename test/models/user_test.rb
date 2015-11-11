443ab958e4bd4319052c6ea62deb676b19ad49e7
Harrison Andrews <harrisonandrews1986@gmail.com>
2015-11-11 16:25:55 -0500

    worked on tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,39 +1,43 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
 end
 
+
+
+
+#   test "username must be present" do
+#     user = User.new
 #
-# test "emails must be present" do
-#   user = User.new
+#     refute user.save
+#     assert user.errors[:name].present?
 #
-#   refute user.save
-#   assert user.errors[:email].present?
+#   end
 #
-# end
+#   test "users must have a password" do
+#     user = User.new
 #
-# test "users must have a password" do
-#   user = User.new
+#     refute user.save
+#     assert user.errors[:password],present?
+#   end
 #
-#   refute user.save
-#   assert user.errors[:password],present?
-# end
+#   test "users with a name and password can be saved" do
+#     user = User.new(name: "Hey Bud", password: "hunter2")
 #
-# test "users with an email and password can be saved" do
-#   user = User.new(email: "foo@bar.com", password: "hunter2")
+#     assert user.save
+#     assert user.errors.empty?
+#   end
 #
-#   assert user.save
-#   assert user.errors.empty?
-# end
-#
-# test "user emails must be unique" do
-#   user1 = User.new(email: "foo@bar.com", password: "cookies")
-#   user2 = User.new(email: "foo@bar.com", password: "monsters")
-#
-#   assert user1.save
-#   assert user2.save
+#   # test "user emails must be unique" do
+#   #   user1 = User.new(email: "foo@bar.com", password: "cookies")
+#   #   user2 = User.new(email: "foo@bar.com", password: "monsters")
+#   #
+#   #   assert user1.save
+#   #   assert user2.save
+#   #
+#   #   end
 #
 # end
